ab889f26a3b0d841498fd125cec7e874f083b426
skyclementem <martina.clemente.reply@skytv.it>
2021-10-08 15:30:19 +0200

    added note-service, fixed db connection

--- configs/db.go
+++ configs/db.go
@@ -10,7 +10,7 @@ import (
 )
 
 func Connect() *gorm.DB {
-	dsn := "host=postgres-service user=niceusr password=nicepwd dbname=notes port=5432 sslmode=disable"
+	dsn := "host=postgres-service user=postgres password=nicepwd dbname=notes port=5432 sslmode=disable"
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
 		log.Printf("Failed to connect")

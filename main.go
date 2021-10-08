5bbb1459c0766a1d48bf886a03b8fdf049fdc2c5
skyclementem <martina.clemente.reply@skytv.it>
2021-10-08 12:03:21 +0200

    beginning Dockerfile

--- main.go
+++ main.go
@@ -3,13 +3,25 @@ package main
 import (
 	config "beelogiq/notes/configs"
 	"beelogiq/notes/routes"
+	"fmt"
 	"log"
 
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
+	// Nice ascii art
+	welcome := `
+███╗░░██╗░█████╗░████████╗███████╗░██████╗
+████╗░██║██╔══██╗╚══██╔══╝██╔════╝██╔════╝
+██╔██╗██║██║░░██║░░░██║░░░█████╗░░╚█████╗░
+██║╚████║██║░░██║░░░██║░░░██╔══╝░░░╚═══██╗
+██║░╚███║╚█████╔╝░░░██║░░░███████╗██████╔╝
+╚═╝░░╚══╝░╚════╝░░░░╚═╝░░░╚══════╝╚═════╝░
 
+	`
+
+	fmt.Println(welcome)
 	// Init Db
 	config.Connect()
 
